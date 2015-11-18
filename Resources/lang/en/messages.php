09e180d0ecc0db585b9ee67c15e8c27b608af2a3
Nicolas Widart <n.widart@gmail.com>
2015-11-18 19:50:47 +0100

    Adding / modifying the notifications message

--- Resources/lang/en/messages.php
+++ Resources/lang/en/messages.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'you have x new notifications' => '{0} There are no notifications|[1,1] You have one new notification|[2,Inf] You have :amount new notifications'
+    'no notifications' => 'There are no notifications.',
 ];
---
+++ Resources/lang/fr/messages.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'no notifications' => 'Il n\'y a pas de notifications.',
+];
