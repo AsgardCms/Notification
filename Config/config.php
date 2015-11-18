62fb3d6f365ae8b122829207aae76c16507b6586
Nicolas Widart <n.widart@gmail.com>
2015-11-18 19:39:25 +0100

    Adding opt-in configuration to enable real time notifications

--- Config/config.php
+++ Config/config.php
@@ -1,5 +1,12 @@
 <?php
 
 return [
-    'name' => 'Notification'
+    /*
+    |--------------------------------------------------------------------------
+    | Real time notifications
+    |--------------------------------------------------------------------------
+    | Setting this to true requires a broadcast setting configured, asgard
+    | comes by default with the Pusher integration (js).
+    */
+    'real-time' => false,
 ];
