8f4266b0479bb6e23b92d8dac32cf663cb81a407
Nicolas Widart <n.widart@gmail.com>
2015-11-19 15:26:56 +0100

    Adding french translations

--- Resources/lang/fr/messages.php
+++ Resources/lang/fr/messages.php
@@ -2,4 +2,14 @@
 
 return [
     'no notifications' => 'Il n\'y a pas de notifications.',
+    'notifications' => 'Notifications',
+    'delete all confirmation' => 'Etes vous sur de vouloir supprimer toutes les notifications ?',
+    'time' => 'Temps',
+    'title' => 'Titre',
+    'message' => 'Message',
+    'is read' => 'Est lu',
+    'mark all as read' => 'Marquer tous comme lu',
+    'delete all' => 'Suprimmer tous',
+    'all notifications deleted' => 'Toutes vous notifications ont été supprimer.',
+    'all notifications marked as read' => 'Toutes vous notifications ont été marquer comme lu.',
 ];
