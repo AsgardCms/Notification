6f2bad3c908d1b1dfb7eda71934edd7a970642ec
Nicolas Widart <n.widart@gmail.com>
2015-11-19 13:38:58 +0100

    Loading the routeSP

---
+++ Providers/RouteServiceProvider.php
@@ -0,0 +1,36 @@
+<?php namespace Modules\Notification\Providers;
+
+use Modules\Core\Providers\RoutingServiceProvider as CoreRoutingServiceProvider;
+
+class RouteServiceProvider extends CoreRoutingServiceProvider
+{
+    /**
+     * The root namespace to assume when generating URLs to actions.
+     * @var string
+     */
+    protected $namespace = 'Modules\Notification\Http\Controllers';
+
+    /**
+     * @return string
+     */
+    protected function getFrontendRoute()
+    {
+        return false;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getBackendRoute()
+    {
+        return false;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getApiRoute()
+    {
+        return __DIR__ . '/../Http/apiRoutes.php';
+    }
+}
