6714c5f6ce368bfa09a4de5df9ae6d27b1628780
Nicolas Widart <n.widart@gmail.com>
2015-11-19 15:01:16 +0100

    Basic translations

--- Resources/lang/en/messages.php
+++ Resources/lang/en/messages.php
@@ -2,4 +2,11 @@
 
 return [
     'no notifications' => 'There are no notifications.',
+    'notifications' => 'Notifications',
+    'delete all confirmation' => 'Are you sure you want to delete all notifications ?',
+    'time' => 'Time',
+    'title' => 'Title',
+    'message' => 'Message',
+    'mark all as read' => 'Mark all as read',
+    'delete all' => 'Delete all',
 ];
