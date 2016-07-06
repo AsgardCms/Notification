da6df5d8b625fb84df4eda6e932d39aaca202462
Nicolas Widart <n.widart@gmail.com>
2016-07-06 21:53:45 +0200

    Adding nicer permission labels

--- Config/permissions.php
+++ Config/permissions.php
@@ -2,9 +2,9 @@
 
 return [
     'notification.notifications' => [
-        'index',
-        'destroy',
-        'destroyAll',
-        'markAllAsRead',
+        'index' => trans('notification::messages.list resource'),
+        'destroy' => trans('notification::messages.destroy resource'),
+        'destroyAll' => trans('notification::messages.destroy all resource'),
+        'markAllAsRead' => trans('notification::messages.markAllAsRead resource'),
     ],
 ];
